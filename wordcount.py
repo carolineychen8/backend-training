93949618a9ff541c280fe4551ef288ab825e1c58
carolineychen8 <64424139+carolineychen8@users.noreply.github.com>
2020-09-23 16:42:20 -0400

    Add files via upload

---
+++ wordcount.py
@@ -0,0 +1,114 @@
+#!/usr/bin/python -tt
+# Copyright 2010 Google Inc.
+# Licensed under the Apache License, Version 2.0
+# http://www.apache.org/licenses/LICENSE-2.0
+
+# Google's Python Class
+# http://code.google.com/edu/languages/google-python-class/
+
+"""Wordcount exercise
+Google's Python class
+
+The main() below is already defined and complete. It calls print_words()
+and print_top() functions which you write.
+
+1. For the --count flag, implement a print_words(filename) function that counts
+how often each word appears in the text and prints:
+word1 count1
+word2 count2
+...
+
+Print the above list in order sorted by word (python will sort punctuation to
+come before letters -- that's fine). Store all the words as lowercase,
+so 'The' and 'the' count as the same word.
+
+2. For the --topcount flag, implement a print_top(filename) which is similar
+to print_words() but which prints just the top 20 most common words sorted
+so the most common word is first, then the next most common, and so on.
+
+Use str.split() (no arguments) to split on all whitespace.
+
+Workflow: don't build the whole program at once. Get it to an intermediate
+milestone and print your data structure and sys.exit(0).
+When that's working, try for the next milestone.
+
+Optional: define a helper function to avoid code duplication inside
+print_words() and print_top().
+
+"""
+
+import sys
+
+# Define print_words(filename) and print_top(filename) functions.
+# You could write a helper utility function that reads a file
+# and builds and returns a word/count dict for it.
+# Then print_words() and print_top() can just call the utility function.
+
+#prints how often each word appears in text
+def print_words(filename):
+
+  #use helper method, build dictionary
+  word_count = build_dict(filename)
+
+  for word in word_count:
+    print(word + " " + str(word_count[word]));
+
+#prints top 20 most common words 
+def print_top(filename):
+  
+  #use helper method, build dictionary
+  word_count = build_dict(filename)
+
+  #sort dictionary by value, highest value first
+  word_count = sorted(word_count.items(), key=lambda tup : tup[1], reverse=True)
+
+  #put 20 most common words into list, print
+  top = []
+  for word in word_count:
+    top.append(word[0])
+    if len(top) >= 20 or len(top) >= len(word_count):
+      break
+    
+  print(top)
+
+#helper method, reads file and returns word/count dictionary
+def build_dict(filename):
+
+  #key = word, value = number of times that word appears in file
+  counts = dict()
+
+  #read file and split by whitespace
+  data = ""
+  with open (filename, "r") as myfile:
+    data = myfile.read()
+  words = data.split()
+
+  #turn all words into lowercase
+  words = [word.lower() for word in words]
+
+  #count number of occurrences of word in list, add to dict
+  for word in words:
+    if word not in counts:
+      counts[word] = words.count(word)
+      
+  return counts
+
+# This basic command line argument parsing code is provided and
+# calls the print_words() and print_top() functions which you must define.
+def main():
+  if len(sys.argv) != 3:
+    print ('usage: ./wordcount.py {--count | --topcount} file')
+    sys.exit(1)
+
+  option = sys.argv[1]
+  filename = sys.argv[2]
+  if option == '--count':
+    print_words(filename)
+  elif option == '--topcount':
+    print_top(filename)
+  else:
+    print ('unknown option: ' + option)
+    sys.exit(1)
+
+if __name__ == '__main__':
+  main()
