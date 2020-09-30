13ddaa0a5281fe7502d310ab4bc0b9b1ced3a7dc
carolineychen8 <64424139+carolineychen8@users.noreply.github.com>
2020-09-29 23:53:09 -0400

    Add files via upload

---
+++ demo_uploadfile.js
@@ -0,0 +1,31 @@
+//https://www.w3schools.com/nodejs/nodejs_uploadfiles.asp
+var http = require('http');
+var formidable = require('formidable');
+var fs = require('fs');
+
+http.createServer(function (req, res) {
+  if (req.url == '/fileupload') {
+    //parse uploaded form
+    var form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+      
+      //move file to appropriate place
+      var oldpath = files.filetoupload.path;
+      var newpath = 'C:/Users/Your Name/' + files.filetoupload.name;
+      fs.rename(oldpath, newpath, function (err) {
+        if (err) throw err;
+        res.write('File uploaded and moved!');
+        res.end();
+      });
+ });
+  } else {
+
+    //write HTML form with upload file option
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+    res.write('<input type="file" name="filetoupload"><br>');
+    res.write('<input type="submit">');
+    res.write('</form>');
+    return res.end();
+  }
+}).listen(8080);
