313252dcebd9ef8cebc0d96f774d707ee2d43d43
carolineychen8 <64424139+carolineychen8@users.noreply.github.com>
2020-09-29 23:21:45 -0400

    Delete http_url.js

--- http_url.js
+++
@@ -1,9 +0,0 @@
-//code from https://www.w3schools.com/nodejs/nodejs_http.asp
-var http = require('http');
-
-//create server object
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write(req.url); //write part of url after domain name
-  res.end();
-}).listen(8080);
